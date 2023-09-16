e71f581665f693f4a004c042656bcafcc951e27b
Aghasaf06 <agasfm90@gmail.com>
2023-09-17 03:30:42 +0400

    Credit Application Home page

---
+++ src/main/java/buta/creditapplication/Controller/UserController.java
@@ -0,0 +1,15 @@
+package buta.creditapplication.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/user")
+public class UserController {
+
+    @GetMapping(path = "/home")
+    public String home() {
+        return "home";
+    }
+}
